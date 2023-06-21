7bba9f568a7b9ff66fee9546a13273194b9a123b
bwrosen20 <bwrosen20@gmail.com>
2023-06-21 19:21:11 -0400

    commit tree a4fdb94ce9b12c627a0516212449b8ec8645dd45

--- ruby/find_first_duplicate.rb
+++ ruby/find_first_duplicate.rb
@@ -1,5 +1,17 @@
+require "pry"
+
 def find_first_duplicate(arr)
-  # type your code in here
+  if arr.uniq==arr
+    return -1
+  else
+    array=[]
+    for e in arr do
+        if array.include?(e) 
+         return e
+        end
+      array << e
+    end
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -14,5 +26,9 @@ if __FILE__ == $PROGRAM_NAME
   # Don't forget to add your own!
 end
 
+
+first=find_first_duplicate([1,2,3,3,4,5])
+second=find_first_duplicate([2,3,4,5])
+binding.pry
 # Please add your pseudocode to this file
 # And a written explanation of your solution
